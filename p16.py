4fc9dc821a8e7f4c55b47590916fb7b174e792fa
Gowt859 <52487557+Gowt859@users.noreply.github.com>
2019-07-08 10:34:24 +0530

    Create p16.py

---
+++ p16.py
@@ -0,0 +1,8 @@
+g1,g2=map(int,input().split())
+for j in range(g1,g2):
+    if j>1:
+        for i in range(2,j):
+            if j%i==0:
+                break
+        else:
+            print(j,end='')
